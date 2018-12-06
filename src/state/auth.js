fb692e4b8d026a7e738b66ca2affefa22a2735ba
kubzer0 <jakub.gryka@gmail.com>
2018-12-06 15:16:20 +0100

    input now displays text from database from individual user

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,4 +1,5 @@
 import { auth, database, googleProvider } from '../firebaseConfig'
+import { loadTextFromDbAsyncAction } from './userData';
 
 const LOG_IN = 'auth/LOG_IN'
 const LOG_OUT = 'auth/LOG_OUT'
@@ -11,6 +12,7 @@ export const initAuthChangeListeningAction = () => (dispatch, getState) => {
             if (user) {
                 dispatch(logInAction(user))
                 dispatch(saveLoginTimestampAsyncAction())
+                dispatch(loadTextFromDbAsyncAction())
             } else {
                 dispatch(logOutAction())
             }
